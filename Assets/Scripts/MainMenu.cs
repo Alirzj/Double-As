a5ac65821617ccebacfb46803c9314c23129f221
Alirzj <jahanialireza7353@gmail.com>
2024-08-03 20:33:08 +0800

    mainMenu Audio

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,15 +5,18 @@ public class MainMenu : MonoBehaviour
     public GameObject mainMenu;
     public GameObject optionsMenu;
     public GameObject creditsMenu;
+    public AudioSource buttons;
 
     public void PlayGame()
     {
+        buttons.Play();
         // Load the game scene (replace "GameScene" with your actual game scene name)
         UnityEngine.SceneManagement.SceneManager.LoadScene("Ali");
     }
 
     public void OpenOptions()
     {
+        buttons.Play();
         // Show options menu and hide main menu
         optionsMenu.SetActive(true);
         mainMenu.SetActive(false);
@@ -21,6 +24,7 @@ public class MainMenu : MonoBehaviour
 
     public void OpenCredits()
     {
+        buttons.Play();
         // Show credits menu and hide main menu
         creditsMenu.SetActive(true);
         mainMenu.SetActive(false);
@@ -28,6 +32,7 @@ public class MainMenu : MonoBehaviour
 
     public void BackToMainMenu()
     {
+        buttons.Play();
         // Show main menu and hide options/credits menu
         mainMenu.SetActive(true);
         optionsMenu.SetActive(false);
@@ -36,9 +41,15 @@ public class MainMenu : MonoBehaviour
 
     public void ExitGame()
     {
+        buttons.Play();
         // Quit the application
         Application.Quit();
         // Note: This will not work in the editor. To test in the editor, you can use:
         // UnityEditor.EditorApplication.isPlaying = false; // Uncomment this line if you are in the editor
+    }
+
+    public void PlayButtonAudio()
+    {
+        buttons.Play();
     }
 }
